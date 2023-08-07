8ac2010bfcf79c3e91bad40a916b7ec213f175b6
Gasanova Tamara <solas93@mail.ru>
2023-08-07 21:34:33 +0300

    feat: finish  towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+    let multidimensionalArray = [];
+    let resultArray = [];
+  
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 == 0) {
+        multidimensionalArray.push(matrix[i]);
+      } else {
+        multidimensionalArray.push(matrix[i].reverse())
+      };
+      {
+        for (let j = 0; j < multidimensionalArray[i].length; j++) {
+          resultArray.push(multidimensionalArray[i][j])
+        }
+      }
+    }
+    return resultArray;
 }
